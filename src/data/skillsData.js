f8b155bb0cadddb717662c1668a7932ca69212d2
CodeyArch <codeyarch123@gmail.com>
2024-11-05 01:53:28 +0000

    Updating Skills And Testing Website Functionality

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -5,11 +5,10 @@ const skillsData = [
     { name: 'CSS', rating: 4 },
     { name: 'JavaScript', rating: 4 },
     { name: 'React', rating: 3 },
-    { name: 'Node.js', rating: 2 },
     { name: 'Python', rating: 3 },
     { name: 'Unity', rating: 4 },
-    { name: 'C#', rating: 3 },
-    { name: 'Java', rating: 3 },
+    { name: 'C#', rating: 4 },
+    { name: 'Java', rating: 5 },
 ];
 
 export default skillsData;
